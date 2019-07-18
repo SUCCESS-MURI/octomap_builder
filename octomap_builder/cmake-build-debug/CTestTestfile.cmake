3f3b82ac0175cd5077716517df34f390766acf03
gregory lemasurier <gregorylemasurier@gmail.com>
2019-07-18 11:40:05 -0400

    remove .idea and cmake debug

--- octomap_builder/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/greg/fetch_ws/src/fetchit/octomap_builder
-# Build directory: /home/greg/fetch_ws/src/fetchit/octomap_builder/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
